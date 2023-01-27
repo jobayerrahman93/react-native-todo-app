9efe7ca47870b3e6020112e3fd5f624334778ba8
jobayercse71 <rahmanjobayer71@gmail.com>
2023-01-27 22:46:00 +0600

    fonts,colors, spacing and typography added

---
+++ src/theme/colors.js
@@ -0,0 +1,15 @@
+export const colors={
+    black:'#070724',
+    white:'#fff',
+    darkGrey:'#38384f',
+    grey:'#838391',
+    lightGreen:'#419EBB',
+    orange:'#EDA249',
+    purple:'#6f2ed6',
+    darkOrange:'#D14c32',
+    darkGrey:'#38384f',
+    red:'#D83A34',
+    lightRed:'#CD5120',
+    green:'#1ec2a4',
+    blue:'#2d68f0',
+}
---
+++ src/theme/typography.js
@@ -0,0 +1,6 @@
+export const typography ={
+    primary:'Poppins-Regular',
+    primaryBold:'Poppins-bold',
+    bold: 'Antonio-Medium'
+
+}
